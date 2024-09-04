98568b396d84b3918f9f3327dfa20d681a649c42
alexandernc0043 <aprechtel0043@gmail.com>
2024-09-04 18:06:57 -0400

    comments

--- Module 3/Lab/ITSC_2181_M03_U1_Lab_801341367/weather.c
+++ Module 3/Lab/ITSC_2181_M03_U1_Lab_801341367/weather.c
@@ -4,10 +4,12 @@ int main(void){
   int hotestDay;
   int hotestTemp;
   printf("You will be asked to enter the daily high temperature for 10 consecutive days.\n");
+  // Loops 10 times asking for temperature
   for(int i = 1; i <= 10; i++){
     printf("Enter a high temperature: ");
     int temp;
     scanf("%d", &temp);
+// if hotter or equal sets hotest day&temp to i and temperature
     if(temp >= hotestTemp){
       hotestTemp = temp;
       hotestDay = i;
