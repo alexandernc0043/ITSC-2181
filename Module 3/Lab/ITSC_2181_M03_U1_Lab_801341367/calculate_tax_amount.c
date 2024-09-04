089bc8c0912ce7c72621c8de42abb1306652c5bc
alexandernc0043 <aprechtel0043@gmail.com>
2024-09-04 18:04:55 -0400

    comments

--- Module 3/Lab/ITSC_2181_M03_U1_Lab_801341367/calculate_tax_amount.c
+++ Module 3/Lab/ITSC_2181_M03_U1_Lab_801341367/calculate_tax_amount.c
@@ -4,6 +4,9 @@ int main(void){
   int income = 0;
   printf("Please enter your income: ");
   scanf("%d",&income);
+  /*
+    * Calculates Tax amount
+    */
   float taxes;
   if(income >= 190150){
     taxes = income * .35;
@@ -17,13 +20,22 @@ int main(void){
     taxes = income * .12;
   }
   printf("Tax due = $%.2f\n",taxes);
+  /*
+    * Determines amount of tax credit
+    */
   printf("Enter the number of dependents (0 for none): ");
   int dependents;
   scanf("%d",&dependents);
   int taxCredit = dependents * 450;
+  /*
+  * if larger then 2250 set it to 2250
+  */
   if(taxCredit > 2250){
     taxCredit = 2250;
   }
+  /*
+    * Final print messages
+    */
   printf("Tax credit = $%d\n",taxCredit);
   printf("Adjusted Tax = $%.2f\n", taxes - taxCredit);
   return 0;
