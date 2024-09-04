454c433f661e536a644cee1a6ecce8aaee902a6b
alexandernc0043 <aprechtel0043@gmail.com>
2024-09-04 10:26:48 -0400

    Working on ITSC_2181_EX-3-1-2_801341367

--- Module 3/ITSC_2181_EX-3-1-2_801341367.c
+++ Module 3/ITSC_2181_EX-3-1-2_801341367.c
@@ -6,5 +6,22 @@ int main(void){
   int c = 8;
   int d = 4;
   int e = 12;
+
+  if(b>c){
+    printf("Part 1 : true\n");
+  } else {
+    printf("Part 1 : false\n");
+  }
+  if((e/d)+6){
+    printf("Part 2 : true\n");
+  } else {
+    printf("Part 2 : false\n");
+  }
+  if(a-= ((b>c) || (e/d)) +6){
+    printf("Part 3 : true\n");
+  } else {
+    printf("Part 3 : false\n");
+  }
+  printf("A = %d\n", a);
   return 0;
 }
