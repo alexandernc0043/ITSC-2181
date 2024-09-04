3fc5fc71519a38de9e48630f803d34f398b2116a
alexandernc0043 <aprechtel0043@gmail.com>
2024-09-04 13:11:15 -0400

    Working on ITSC_2181_EX-3-1-6_801341367

---
+++ Module 3/ITSC_2181_EX-3-1-6_801341367.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+  int input = 0;
+  int sum = 0;
+  while(1==1){
+    printf("Current Sum: %d\n", sum);
+    printf("Please enter the next number: ");
+    scanf("%d",&input);
+    if(input == -1){
+      break;
+    }
+    sum += input;
+  }
+  printf("Done!\n");
+}
