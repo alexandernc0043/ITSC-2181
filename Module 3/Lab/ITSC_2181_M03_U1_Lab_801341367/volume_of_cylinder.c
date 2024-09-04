9aefce4e07c2feff2652e979f00a7f0171d55d9e
alexandernc0043 <aprechtel0043@gmail.com>
2024-09-04 18:05:59 -0400

    comments

--- Module 3/Lab/ITSC_2181_M03_U1_Lab_801341367/volume_of_cylinder.c
+++ Module 3/Lab/ITSC_2181_M03_U1_Lab_801341367/volume_of_cylinder.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(void){
+  // variables
   float pi = 3.14159;
   float radius;
   float height;
@@ -8,6 +9,7 @@ int main(void){
   scanf("%f", &radius);
   printf("Enter height: ");
   scanf("%f",&height);
+  // calculates volume based of formula (pi)(radius)^2(height)
   float volume = pi * (radius*radius) * height;
   printf("Volume: %.2f\n", volume);
   return 0;
