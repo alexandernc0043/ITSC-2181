a444158a9a04c7fe4e9b3f4be0a23cd906026f6e
alexandernc0043 <aprechtel0043@gmail.com>
2024-09-04 10:06:40 -0400

    Fixed

--- Module 3/ITSC_2181_EX3-1_801341367.c
+++ Module 3/ITSC_2181_EX3-1_801341367.c
@@ -7,10 +7,9 @@ int main(void){
   float myFloat;
   int num;
   scanf("%c%c %f %d", &char1, &char2, &myFloat, &num);
-  printf("\n");
   printf("%c%c\n", char1, char2);
   printf("%f\n", myFloat);
   printf("Two Decimal places: %.2f\n", myFloat);
-  printf("%d", num);
+  printf("%d\n", num);
   return 0;
 }
