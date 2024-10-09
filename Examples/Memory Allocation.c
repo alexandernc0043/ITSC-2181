04888d13683da17638511c80b9683ae5554f9ccc
alexandernc0043 <aprechtel0043@gmail.com>
2024-10-09 09:13:48 -0400

    Memory Allocation Example

---
+++ Examples/Memory Allocation.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main(void)
+{
+	// create a pointer called "ptr" 
+	//and cast it to int pointer
+	// times the sizeof int (4 bytes) by 5
+	int *ptr = (int*) malloc(sizeof(int) * 5);
+
+	if(ptr == NULL){
+		printf("Error!\n");
+	} 
+	else {
+		printf("Success!\n");
+	}
+
+	printf("Memory Address: %p\n",ptr);
+
+	return 0;
+}
