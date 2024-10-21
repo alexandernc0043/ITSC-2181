98e0466911959c5a774f5bbff236d28f2a6a92ad
alexandernc0043 <aprechtel0043@gmail.com>
2024-10-21 09:51:32 -0400

    renamed files
    modified file a bit
    
    forgot to rename

---
+++ Modules/Module 6/Unit 1/Exercise 2/Question 3.asm
@@ -0,0 +1,9 @@
+# Using ONLY the add, sub and slli instruction to convert the following C statement to the corresponding RISC-V assembly. 
+# Assume that the variables f, g, and j are integers assigned to registers t0, t1, and t2 respectively. 
+# You can use other temporary registers such as t3, t4, t5, t6, etc.
+
+ 
+
+# f = g * 5 – j * 32;
+
+
--- Modules/Module 6/Unit 1/Exercise 2/Question 3.s
+++
@@ -1,8 +0,0 @@
-Using ONLY the add, sub and slli instruction to convert the following C statement to the corresponding RISC-V assembly. Assume that the variables f, g, and j are integers assigned to registers t0, t1, and t2 respectively. You can use other temporary registers such as t3, t4, t5, t6, etc.
-
- 
-
-f = g * 5 – j * 32;
-
-asm :
-
