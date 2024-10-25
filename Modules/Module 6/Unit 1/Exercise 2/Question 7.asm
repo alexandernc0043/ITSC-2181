0803b022a954f652810178c7edea22645115efef
alexandernc0043 <aprechtel0043@gmail.com>
2024-10-25 09:12:41 -0400

    done with assignment

--- Modules/Module 6/Unit 1/Exercise 2/Question 7.asm
+++ Modules/Module 6/Unit 1/Exercise 2/Question 7.asm
@@ -21,9 +21,6 @@
 add t3, t2, x0		# t3 = j
 slli t2, t2, 5		# j * 32
 add t2, t2, t3		# j + j
-
 sub t1, t1, t2		# g = g - j -> (j * 33)
-
 slli t4, t0, 1 		# t4 = f * 2
-
 add t0, t1, t4		# f = g + t4 -> (f * 2)
