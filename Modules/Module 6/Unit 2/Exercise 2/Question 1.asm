bd270b30772ad00cca1a63513d69d2d63ae02fc1
alexandernc0043 <aprechtel0043@gmail.com>
2024-11-02 17:53:33 -0400

    Questions 1 and 2

--- Modules/Module 6/Unit 2/Exercise 2/Question 1.asm
+++ Modules/Module 6/Unit 2/Exercise 2/Question 1.asm
@@ -1,9 +1,9 @@
 # a = b * 3 + i * 4;
 
-lw x2, 0x12			# Load b, size of word
-lw x5, 0x15			# load i, size of word
-slliw x22, x2, 2 	# b * 4
-subw x22, x22, x2 	# x22 = b * 3
-slliw x25, x5, 2 	# x25 = i * 4
-addw x1, x22, x25 	# x1 = (b * 3) + (i * 4)
-sw x1, 0x11			# store 0x11 in x1
+ld x2, 0(x12)			# Load b
+ld x5, 0(x15)			# load i
+slli x22, x2, 2 		# x22 = b * 4
+sub x22, x22, x2 		# x22 = b - b (b*3)
+slli x25, x5, 2 		# x25 = i * 4
+add x1, x22, x25 		# x1 = (b * 3) + (i * 4)
+sw x1, 0(x11)			# store 
