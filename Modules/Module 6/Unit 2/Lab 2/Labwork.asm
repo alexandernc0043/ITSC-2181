614449095a27b2060f64507e92c56c6fe985d37a
Alexander P <144269917+alexandernc0043@users.noreply.github.com>
2024-11-03 13:07:29 -0500

    Create Labwork.asm

---
+++ Modules/Module 6/Unit 2/Lab 2/Labwork.asm
@@ -0,0 +1,21 @@
+.data
+	A: .word 10,11,12,13
+	sum: .word 0
+.text
+main:
+	la t0, A
+	
+	lw t1, 0(t0)	# Load A[0] into t1
+	lw t2, 4(t0)	# Load A[1] into t2
+	lw t3, 8(t0)	# Load A[2] into t3
+	lw t4, 12(t0)	# Load A[3] into t4
+	
+	add t5, t1, t2	# Add t5 = t1 + t2
+	add t5, t5, t3	# Add t5 = t5 + t3
+	add t5, t5, t4	# Add t5 = t5 + t4
+	
+	la t6, sum	# Load sum into t6
+	sw t5, 0(t6)	# Store Sum into t6
+	
+	li a7, 10	# Exit Program
+	ecall
