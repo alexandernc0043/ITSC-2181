4f2b7fb6b187f69c8eae2c2b6d8759abb414d790
alexandernc0043 <aprechtel0043@gmail.com>
2024-11-08 10:28:23 -0500

    Workin

--- Modules/Module 6/Unit 3/Lab/Program 3.s
+++ Modules/Module 6/Unit 3/Lab/Program 3.s
@@ -1,39 +1,45 @@
-.data
-	arr: .space 400
-	newLine: .string "\n"
-	sum: .word 0
-.text 
-.globl main
+	.data
+arr:
+	.space  400
+sum:
+	.word   0
+newLine:
+	.string "\n"
+
+	.text
+	.globl  main
 main:
-	la 			t0, arr				# Load address of arr
-	li 			t1, 0				# i
-	li 			t2, 100				# max number	
-loop:
-	slli 		t3, t1, 2 			# i * 4
-	add 		t4, t0, t3			# get correct address of arr[i]
-	lw 			t5, 0(t4)			# load current index
-    jal 		random
-    add 		t5, x0, a0          # Generated random number to t5
-    sw 			t5, 0(t4)            # Store the random number in arr[i]
-    jal 		print
-    addi 		t1, t1, 1 			# increment
-	bne 		t1, t2, loop		# while i != 100 do loop
-	bne 		t1, zero, success
-print:
-	add 		a0, x0, t5 			# t1nt to print
-    li 			a7, 1  				# printt1nt
-    ecall
-    la 			a0, newLine 		# Print Newline
-    li 			a7, 4  				# Print String ecall
-    ecall
-    jr ra 
+	la      t0, arr           # Load address of arr
+	li      t1, 0             # i
+	li      t2, 100           # max number
+	la      t6, sum           # load sum
+	lw      s0, 0(t6)         # sum loaded into s0
+loopOne:
+	slli    t3, t1, 2         # i * 4
+	add     t4, t0, t3        # get correct address of arr[i]
+	lw      t5, 0(t4)         # load current index
+	jal     random
+	add     t5, x0, a0        # Generated random number to t5
+	sw      t5, 0(t4)         # Store the random number in arr[i]
+	addi    t1, t1, 1         # increment
+	bne     t1, t2, loopOne   # while i != 100 do loop
+	li      t1,0              # set i = 0
+	bne     t1, zero, loopTwo # jump to loopTwo
+loopTwo:
+	slli    t3, t1, 2         # i * 4
+	add     t4, t0, t3        # address of arr[i]
+	lw      t5, 0(t4)         # load current index
+	add     s0, s0, t5        # add to sum
+	addi    t1,t1, 1          # increment i
+	bne,    t1, t2, loopTwo   # loop if i != 100
+	bne,    t1, zero, success # if done jump to sucess
 random:
-	li 			a0, 1                # Lower bound 1
-    li 			a1, 100              # Upper bound 100
-    li 			a7, 42 				 # ecall to generate random number
-    ecall
-    jr 			ra
+	li      a0, 1             # Lower bound 1
+	li      a1, 100           # Upper bound 100
+	li      a7, 42            # ecall to generate random number
+	ecall
+	jr      ra
 success:
-	li a0, 0
-	li a7, 93
+	li      a0, 0
+	li      a7, 93
 	ecall
